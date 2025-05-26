418f12f293b22d015f07dcab59c255935cc18749
Vladislav <10328026@stud.op.edu.ua>
2025-05-26 13:12:54 +0300

    add hw9

---
+++ HW_9/HW9/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace HW9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtMovieTitle;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.WebBrowser webResult;
+        private System.Windows.Forms.Label lblMovieTitle;
+        private System.Windows.Forms.Button btnSend;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtMovieTitle = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.webResult = new System.Windows.Forms.WebBrowser();
+            this.lblMovieTitle = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // lblMovieTitle
+            this.lblMovieTitle.AutoSize = true;
+            this.lblMovieTitle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblMovieTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblMovieTitle.Text = "Enter the name of the movie:";
+
+            // txtMovieTitle
+            this.txtMovieTitle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtMovieTitle.Location = new System.Drawing.Point(200, 17);
+            this.txtMovieTitle.Size = new System.Drawing.Size(300, 25);
+
+            // btnSearch
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnSearch.Location = new System.Drawing.Point(510, 16);
+            this.btnSearch.Size = new System.Drawing.Size(100, 28);
+            this.btnSearch.Text = "Search";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+
+            // webResult
+            this.webResult.Location = new System.Drawing.Point(20, 60);
+            this.webResult.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webResult.Size = new System.Drawing.Size(590, 300);
+
+            // btnSend
+            this.btnSend.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnSend.Location = new System.Drawing.Point(20, 370);
+            this.btnSend.Size = new System.Drawing.Size(200, 35);
+            this.btnSend.Text = "Send to Email";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(640, 420);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.webResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtMovieTitle);
+            this.Controls.Add(this.lblMovieTitle);
+            this.Name = "Form1";
+            this.Text = "Movie Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ HW_9/HW9/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace HW9
+{
+    public partial class Form1 : Form
+    {
+        private string movieHtml = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string title = txtMovieTitle.Text.Trim();
+            if (string.IsNullOrEmpty(title))
+            {
+                MessageBox.Show("Please enter a movie title.");
+                return;
+            }
+
+            string apiKey = "9bb30534"; 
+            string url = $"http://www.omdbapi.com/?t={Uri.EscapeDataString(title)}&apikey={apiKey}";
+
+            using var client = new HttpClient();
+
+            try
+            {
+                string response = await client.GetStringAsync(url);
+                using var doc = JsonDocument.Parse(response);
+                var root = doc.RootElement;
+
+                if (root.GetProperty("Response").GetString() == "True")
+                {
+                    string movieTitle = root.GetProperty("Title").GetString();
+                    string year = root.GetProperty("Year").GetString();
+                    string genre = root.GetProperty("Genre").GetString();
+                    string director = root.GetProperty("Director").GetString();
+                    string plot = root.GetProperty("Plot").GetString();
+
+                    movieHtml = $@"
+                    <html><body style='font-family:Segoe UI;'>
+                        <h2>{movieTitle} ({year})</h2>
+                        <p><b>Genre:</b> {genre}</p>
+                        <p><b>Director:</b> {director}</p>
+                        <p><b>Plot:</b> {plot}</p>
+                    </body></html>";
+
+                    webResult.DocumentText = movieHtml;
+                }
+                else
+                {
+                    MessageBox.Show("Movie not found.");
+                    webResult.DocumentText = "";
+                    movieHtml = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(movieHtml))
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+
+            try
+            {
+                var senderEmail = "v.melenchuk03@gmail.com";
+                var senderPassword = "************"; 
+                var receiverEmail = "sunmeatrich@gmail.com";
+
+                var mail = new MailMessage();
+                mail.From = new MailAddress(senderEmail);
+                mail.To.Add(receiverEmail);
+                mail.Subject = "Movie Info Report";
+                mail.Body = movieHtml;
+                mail.IsBodyHtml = true;
+
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(senderEmail, senderPassword),
+                    EnableSsl = true
+                };
+
+                smtpClient.Send(mail);
+                MessageBox.Show("Sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to send: " + ex.Message);
+            }
+        }
+    }
+}
